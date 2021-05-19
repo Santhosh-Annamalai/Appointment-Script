b91cdf5e4f776686e3e2f1a65143c026ca7640d1
Santhosh <34129975+Santhosh-Annamalai@users.noreply.github.com>
2021-05-20 02:06:42 +0530

    Update to use config.json

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const dateObject = new Date();
-const date = dateObject.toLocaleString('en-GB', { timeZone: 'Asia/Kolkata' }).split(",")[0];
+const date = dateObject.toLocaleString("en-GB", { timeZone: "Asia/Kolkata" }).split(",")[0];
 console.log(date, dateObject.toLocaleString());
 let trialCounter = 0;
 const superagent = require("superagent");
-const player = require('play-sound')();
+const player = require("play-sound")();
 const { inspect } = require("util");
+const { dosageProperty, vaccineName, fee, age, districtID } = require("./config.json");
 
 function playAlert() {
-  player.play('./Audio.mp3', (err) => {
+  player.play("./Audio.mp3", (err) => {
     if (err) {
       console.log(`Could not play sound: ${inspect(err)}`);
     }
@@ -16,34 +17,38 @@ function playAlert() {
 
 async function getAppointmentDetails(date) {
   try {
-    const response = await superagent.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=571&date=${date}`);
-  
+    const response = await superagent.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${districtID}&date=${date}`);
     const responseObject = response.body.centers;
     const filteredResponse = responseObject.filter((arrayElement) => {
-        const possibleSessions = arrayElement.sessions.filter((sessionElement) => ((sessionElement.min_age_limit < 45) && (sessionElement.vaccine === "COVAXIN") && (sessionElement.available_capacity > 0)));
+      if (arrayElement.fee_type === fee) {
+        const possibleSessions = arrayElement.sessions.filter(({ min_age_limit, vaccine, available_capacity_dose1, available_capacity_dose2 }) => ((min_age_limit === age) && (vaccine === vaccineName) && ((dosageProperty === "dose2") ? (available_capacity_dose2 > 0) : (available_capacity_dose1 > 0))));
         if (possibleSessions.length > 0) {
           return true;
         }
         else {
           return false;
         }
+      }
+      else {
+        return false;
+      }
     });
     if (filteredResponse.length > 0) {
       return [true, filteredResponse];
     }
     else {
-      return false;
+      return [false];
     }
   }
   catch (error) {
     console.log(inspect(error));
     playAlert();
     const finalRes = new Promise((resolve, reject) => {
-        setTimeout(() => {
+      setTimeout(() => {
         getAppointmentDetails(date).then(res => resolve(res)).catch(err => {
-            console.log(inspect(err));
-            playAlert();
-            reject(err);
+          console.log(inspect(err));
+          playAlert();
+          reject(err);
         });
       }, 0); // https://stackoverflow.com/a/20999077/10901309
     });
@@ -55,7 +60,7 @@ async function loopQuery() {
   try {
     const responseBoolean = await getAppointmentDetails(date);
     if (responseBoolean[0] === true) {
-      console.log(inspect(responseBoolean[1]));
+      console.log(inspect(responseBoolean[1], depth = 4));
       playAlert();
       return "Slots are available. Kindly Proceed for Booking Appointment.";
     }
